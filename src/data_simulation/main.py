af1983f06b07ec8c7b487a2b2ab1f156481d55cd
willianmarquees <willianmarquees@gmail.com>
2024-10-07 21:30:10 -0300

    wip: add temp and heart rate anomalies

--- src/data_simulation/main.py
+++ src/data_simulation/main.py
@@ -1,4 +1,5 @@
 from perlin_noise import PerlinNoise
+import random
 
 print("Module loaded: data_simulation.main")
 
@@ -6,19 +7,27 @@ noise = PerlinNoise(octaves=4, seed=1)
 noiseX = PerlinNoise(octaves=6)
 noiseZ = PerlinNoise(octaves=12)
 
+FEVER_TRIGGER_PCT = 0.083
 MIN_TEMP = 37.5
 MAX_TEMP = 39.3
+TACHYCARDIA_TRIGGER_PCT = 0.022
 MIN_HEARTRATE = 60
 MAX_HEARTRATE = 80
 
-def next_temp(i):
-    temp = MIN_TEMP + (MAX_TEMP - MIN_TEMP) * noise([i / 10])
+def next_temp(i):    
+    if random.random() < FEVER_TRIGGER_PCT:  
+        temp = MAX_TEMP + random.uniform(1, 2.2)  
+    else:
+        temp = MIN_TEMP + (MAX_TEMP - MIN_TEMP) * noise([i / 10])
     return round(temp, 2)
 
 def next_heartrate(i):
-    heartrate = MIN_HEARTRATE + (MAX_HEARTRATE - MIN_HEARTRATE) * noise([i / 10])
+    if random.random() < TACHYCARDIA_TRIGGER_PCT:
+        heartrate = MAX_HEARTRATE + random.uniform(5, 40)
+    else:
+        heartrate = MIN_HEARTRATE + (MAX_HEARTRATE - MIN_HEARTRATE) * noise([i / 10])
     bpm = round(heartrate)
-    return bpm
+    return round(bpm, 2)
 
 def map_range(value, start1, stop1, start2, stop2):
     return start2 + (stop2 - start2) * ((value - start1) / (stop1 - start1))
@@ -33,3 +42,4 @@ def next_geo2d(i, interval):
     lon = round(lon, 4)
 
     return lat, lon
+
