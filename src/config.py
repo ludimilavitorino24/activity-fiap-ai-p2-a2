d76613ee6c72db331f94bf3295a375c32498967a
willianmarquees <140867877+willianmarquees@users.noreply.github.com>
2024-10-07 21:32:32 -0300

    Add data simulation (#31)
    * wip: generate temp from iteration config
    
    Co-authored-by: willianmarquees <willianmarquees@users.noreply.github.com>
    
    * wip: add db populate error handling
    
    Co-authored-by: willianmarquees <willianmarquees@users.noreply.github.com>
    
    * wip: generate geo coords
    
    Co-authored-by: willianmarquees <willianmarquees@users.noreply.github.com>, brunoconterato <brunoconterato@users.noreply.github.com>
    
    * wip: save default config
    
    * wip: improve next_geo2d to be more realistic
    
    Co-authored-by: Bruno Conterato <brunoconterato@users.noreply.github.com>
    
    * wip: add temp and heart rate anomalies
    
    ---------
    
    Co-authored-by: Luis Emidio <luisfuturist@gmail.com>
    Co-authored-by: willianmarquees <willianmarquees@users.noreply.github.com>
    Co-authored-by: willianmarquees <willianmarquees@users.noreply.github.com>, brunoconterato <brunoconterato@users.noreply.github.com>

---
+++ src/config.py
@@ -0,0 +1,41 @@
+import json
+import os
+
+#region Default Config
+
+DEFAULT_CONFIG = {
+    "simulation": {
+        "animalNumber": 2,
+        "interval": 5,
+        "iterations": 4
+    }
+}
+
+def saveDefaultConfig():
+    try:
+        with open("config.json", "x") as f:
+            # beautify
+            f.write(json.dumps(DEFAULT_CONFIG, indent=4))
+    except FileExistsError:
+        pass
+
+saveDefaultConfig()
+
+#endregion
+
+#region Config
+
+config = DEFAULT_CONFIG
+
+with open("config.json", "r") as f:
+    config = json.load(f)
+
+#endregion
+
+#region Values
+
+animalNumber = config.get('simulation', {}).get('animalNumber', DEFAULT_CONFIG['simulation']['animalNumber'])
+interval = config.get('simulation', {}).get('interval', DEFAULT_CONFIG['simulation']['interval'])
+iterations = config.get('simulation', {}).get('iterations', DEFAULT_CONFIG['simulation']['iterations'])
+
+#endregion
