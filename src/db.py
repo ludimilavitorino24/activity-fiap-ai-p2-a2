34dda2228881a0095d583e3562b4139a3cd54057
Roberto Besser <roberto.besser@stratesys-ts.com>
2024-10-09 13:35:11 -0300

    Atualização da integração

--- src/db.py
+++ src/db.py
@@ -1,7 +1,6 @@
 from sqlalchemy import (
-    Column, Integer, String, Date, Numeric, ForeignKey, Sequence, create_engine, schema, Identity
+    Column, Integer, String, Date, Numeric, ForeignKey, Boolean, TIMESTAMP, Identity, create_engine
 )
-from sqlalchemy.dialects.oracle import (VARCHAR2, NUMBER, TIMESTAMP)
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy.sql import func
 
@@ -24,107 +23,97 @@ class Species(Base):
     __tablename__ = 't_wc_species'
     __table_args__ = {'schema': SCHEMA }
 
-    # id_seq = Sequence('id_seq', start=1, increment=3)
+    id_species = Column(Integer, Identity(start=1), primary_key=True, autoincrement=True)
+    name = Column(String(50), nullable=False)
+    is_active = Column(Boolean, nullable=False, default=True)
+    created_at = Column(TIMESTAMP, nullable=False, server_default=func.now())
+    updated_at = Column(TIMESTAMP, nullable=True, server_default=func.now())
 
-    # id_species = Column(NUMBER(38,0), id_seq, server_default=id_seq.next_value(), primary_key=True)
-    id_species = Column(NUMBER(38,0), Identity(start=1), primary_key=True)
-    name = Column(VARCHAR2(50), nullable=False)
-    # is_active = Column(NUMBER(1,0), nullable=False, server_default=1)
-    # created_at = Column(TIMESTAMP, nullable=False, server_default=func.now())
-    # updated_at = Column(TIMESTAMP, nullable=True, server_default=func.now())
-
-    # id_species = Column(Numeric, Identity(start=1), primary_key=True)
-    # id_species = Column(Numeric(38), primary_key=True)
-    # name = Column(String(50), nullable=False)
-    # is_active = Column(Numeric, nullable=False, server_default=1)
-    # created_at = Column(TIMESTAMP, nullable=False, server_default=func.now())
-    # updated_at = Column(TIMESTAMP, nullable=True, server_default=func.now())
-
-    # animals = relationship('Animal', back_populates='species')
+    animals = relationship('Animal', back_populates='species')
 
 
-# # t_wc_breeds table
-# class Breed(Base):
-#     __tablename__ = 't_wc_breeds'
-#     __table_args__ = {'schema': SCHEMA }
+# t_wc_breeds table
+class Breed(Base):
+    __tablename__ = 't_wc_breeds'
+    __table_args__ = {'schema': SCHEMA }
 
-#     id_breed = Column(Integer, primary_key=True, autoincrement=True)
-#     name = Column(String(50), nullable=False)
-#     is_active = Column(Boolean, nullable=False, default=True)
-#     created_at = Column(TIMESTAMP, nullable=False, server_default=func.now())
-#     updated_at = Column(TIMESTAMP, nullable=False, server_default=func.now())
+    id_breed = Column(Integer, Identity(start=1), primary_key=True, autoincrement=True)
+    name = Column(String(50), nullable=False)
+    is_active = Column(Boolean, nullable=False, default=True)
+    created_at = Column(TIMESTAMP, nullable=False, server_default=func.now())
+    updated_at = Column(TIMESTAMP, nullable=True, server_default=func.now())
 
-#     animals = relationship('Animal', back_populates='breed')
-
-
-# # t_wc_animals table
-# class Animal(Base):
-#     __tablename__ = 't_wc_animals'
-#     __table_args__ = {'schema': SCHEMA }
-
-#     id_animal = Column(Integer, primary_key=True, autoincrement=True)
-#     tag_id = Column(Integer, nullable=False)
-#     name = Column(String(50), nullable=False)
-#     birth_date = Column(Date)
-#     id_species = Column(Integer, ForeignKey('t_wc_species.id_species'), nullable=False)
-#     id_breed = Column(Integer, ForeignKey('t_wc_breeds.id_breed'), nullable=False)
-#     is_deleted = Column(Boolean, nullable=False, default=False)
-#     created_at = Column(TIMESTAMP, nullable=False, server_default=func.now())
-#     updated_at = Column(TIMESTAMP, nullable=False, server_default=func.now())
-
-#     species = relationship('Species', back_populates='animals')
-#     breed = relationship('Breed', back_populates='animals')
-#     collars = relationship('AnimalCollar', back_populates='animal')
-
-#     __str__ = lambda self: f'{self.name} ({self.tag_id})'
+    animals = relationship('Animal', back_populates='breed')
 
 
-# # t_wc_collars table
-# class Collar(Base):
-#     __tablename__ = 't_wc_collars'
-#     __table_args__ = {'schema': SCHEMA }
+# t_wc_animals table
+class Animal(Base):
+    __tablename__ = 't_wc_animals'
+    __table_args__ = {'schema': SCHEMA }
 
-#     id_collar = Column(Integer, primary_key=True, autoincrement=True)
-#     is_deleted = Column(Boolean, nullable=False, default=False)
-#     created_at = Column(TIMESTAMP, nullable=False, server_default=func.now())
-#     updated_at = Column(TIMESTAMP, nullable=False, server_default=func.now())
+    id_animal = Column(Integer, Identity(start=1), primary_key=True, autoincrement=True)
+    tag_id = Column(Integer, nullable=False)
+    name = Column(String(50), nullable=False)
+    birth_date = Column(Date)
+    id_species = Column(Integer, ForeignKey(f'{SCHEMA}.t_wc_species.id_species'), nullable=False)
+    id_breed = Column(Integer, ForeignKey(f'{SCHEMA}.t_wc_breeds.id_breed'), nullable=False)
+    is_deleted = Column(Boolean, nullable=False, default=False)
+    created_at = Column(TIMESTAMP, nullable=False, server_default=func.now())
+    updated_at = Column(TIMESTAMP, nullable=True, server_default=func.now())
 
-#     animal_collars = relationship('AnimalCollar', back_populates='collar')
+    species = relationship('Species', back_populates='animals')
+    breed = relationship('Breed', back_populates='animals')
+    collars = relationship('AnimalCollar', back_populates='animal')
 
-
-# # t_wc_animals_collars table
-# class AnimalCollar(Base):
-#     __tablename__ = 't_wc_animals_collars'
-#     __table_args__ = {'schema': SCHEMA }
-
-#     id_animal_collar = Column(Integer, primary_key=True, autoincrement=True)
-#     id_collar = Column(Integer, ForeignKey('t_wc_collars.id_collar'), nullable=False)
-#     id_animal = Column(Integer, ForeignKey('t_wc_animals.id_animal'), nullable=False)
-#     is_deleted = Column(Boolean, nullable=False, default=False)
-#     created_at = Column(TIMESTAMP, nullable=False, server_default=func.now())
-#     updated_at = Column(TIMESTAMP, nullable=False, server_default=func.now())
-
-#     collar = relationship('Collar', back_populates='animal_collars')
-#     animal = relationship('Animal', back_populates='collars')
-#     datalog = relationship('DataLog', back_populates='animal_collar')
+    __str__ = lambda self: f'{self.name} ({self.tag_id})'
 
 
-# # t_wc_datalog table
-# class DataLog(Base):
-#     __tablename__ = 't_wc_datalog'
-#     __table_args__ = {'schema': SCHEMA }
+# t_wc_collars table
+class Collar(Base):
+    __tablename__ = 't_wc_collars'
+    __table_args__ = {'schema': SCHEMA }
 
-#     id_datalog = Column(Integer, primary_key=True, autoincrement=True)
-#     id_animal_collar = Column(Integer, ForeignKey('t_wc_animals_collars.id_animal_collar'), nullable=False)
-#     temperature = Column(Numeric)
-#     heartrate = Column(Integer)
-#     latitude = Column(Numeric)
-#     longitude = Column(Numeric)
-#     created_at = Column(TIMESTAMP, nullable=False, server_default=func.now())
-#     updated_at = Column(TIMESTAMP, nullable=False, server_default=func.now())
-#     is_outlier = Column(Boolean, nullable=False, default=False)
+    id_collar = Column(Integer, Identity(start=1), primary_key=True, autoincrement=True)
+    is_deleted = Column(Boolean, nullable=False, default=False)
+    created_at = Column(TIMESTAMP, nullable=False, server_default=func.now())
+    updated_at = Column(TIMESTAMP, nullable=True, server_default=func.now())
 
-#     animal_collar = relationship('AnimalCollar', back_populates='datalog')
+    animal_collars = relationship('AnimalCollar', back_populates='collar')
+
+
+# t_wc_animals_collars table
+class AnimalCollar(Base):
+    __tablename__ = 't_wc_animals_collars'
+    __table_args__ = {'schema': SCHEMA }
+
+    id_animal_collar = Column(Integer, Identity(start=1), primary_key=True, autoincrement=True)
+    id_collar = Column(Integer, ForeignKey(f'{SCHEMA}.t_wc_collars.id_collar'), nullable=False)
+    id_animal = Column(Integer, ForeignKey(f'{SCHEMA}.t_wc_animals.id_animal'), nullable=False)
+    is_deleted = Column(Boolean, nullable=False, default=False)
+    created_at = Column(TIMESTAMP, nullable=False, server_default=func.now())
+    updated_at = Column(TIMESTAMP, nullable=True, server_default=func.now())
+
+    collar = relationship('Collar', back_populates='animal_collars')
+    animal = relationship('Animal', back_populates='collars')
+    datalog = relationship('DataLog', back_populates='animal_collar')
+
+
+# t_wc_datalog table
+class DataLog(Base):
+    __tablename__ = 't_wc_datalog'
+    __table_args__ = {'schema': SCHEMA }
+
+    id_datalog = Column(Integer, Identity(start=1), primary_key=True, autoincrement=True)
+    id_animal_collar = Column(Integer, ForeignKey(f'{SCHEMA}.t_wc_animals_collars.id_animal_collar'), nullable=False)
+    temperature = Column(Numeric)
+    heartrate = Column(Integer)
+    latitude = Column(Numeric)
+    longitude = Column(Numeric)
+    created_at = Column(TIMESTAMP, nullable=False, server_default=func.now())
+    updated_at = Column(TIMESTAMP, nullable=True, server_default=func.now())
+    is_outlier = Column(Boolean, nullable=True, default=False)
+
+    animal_collar = relationship('AnimalCollar', back_populates='datalog')
 
 #endregion
 
--- src/populate_db.py
+++ src/populate_db.py
@@ -2,13 +2,12 @@ import dotenv
 dotenv.load_dotenv()
 
 from data_simulation.main import next_temp, next_heartrate, next_geo2d
-# from db import engine, Collar, Animal, DataLog, AnimalCollar, Breed, Species
-from db import engine, Species
+from db import engine, Collar, Animal, DataLog, AnimalCollar, Breed, Species
 from sqlalchemy.orm import sessionmaker
 from config import animalNumber, interval, iterations
 from datetime import datetime
 import random
-# from data_simulation.animal_names import cow_names_pt_br
+from data_simulation.animal_names import cow_names_pt_br
 
 def populate_db():
     try:
@@ -23,35 +22,35 @@ def populate_db():
         session.add(new_species)
         # session.commit()
 
-        # new_breed = Breed(
-        #     name="Holstein"
-        # )
-        # session.add(new_breed)
+        new_breed = Breed(
+            name="Holstein"
+        )
+        session.add(new_breed)
         # session.commit()
 
         #endregion
 
-        # for i in range(animalNumber):
-        #     animal_name = random.choice(cow_names_pt_br)
-        #     new_animal = Animal(
-        #         tag_id=1,
-        #         name=animal_name + " " + str(i + 1),
-        #         id_species=new_species.id_species,
-        #         id_breed=new_breed.id_breed
-        #     )
-        #     session.add(new_animal)
-        #     session.commit()
+        for i in range(animalNumber):
+            animal_name = random.choice(cow_names_pt_br)
+            new_animal = Animal(
+                tag_id=1,
+                name=animal_name + " " + str(i + 1),
+                id_species=new_species.id_species,
+                id_breed=new_breed.id_breed
+            )
+            session.add(new_animal)
+            # session.commit()
 
-        #     new_collar = Collar()
-        #     session.add(new_collar)
-        #     session.commit()
+            new_collar = Collar()
+            session.add(new_collar)
+            # session.commit()
 
-        #     new_animal_collar = AnimalCollar(
-        #         id_collar=new_collar.id_collar,
-        #         id_animal=new_animal.id_animal
-        #     )
-        #     session.add(new_animal_collar)
-        #     session.commit()
+            new_animal_collar = AnimalCollar(
+                id_collar=new_collar.id_collar,
+                id_animal=new_animal.id_animal
+            )
+            session.add(new_animal_collar)
+            # session.commit()
             
         #     for j in range(iterations):
         #         temp = next_temp((i + 1) * (j + 1))
@@ -75,15 +74,15 @@ def populate_db():
         #             created_at=formatted_time
         #         )
         #         session.add(new_datalog)
-        #         session.commit()
+        #         # session.commit()
 
-        # print("Farm populated successfully with:")
-        # print("1 species,")
-        # print("1 breed,")
-        # print(f"{animalNumber} animals,")
-        # print(f"{animalNumber} collars,")
-        # print(f"{animalNumber} animal-collar links.")
-        # print(f"{animalNumber * iterations} data logs.")
+        print("Farm populated successfully with:")
+        print("1 species,")
+        print("1 breed,")
+        print(f"{animalNumber} animals,")
+        print(f"{animalNumber} collars,")
+        print(f"{animalNumber} animal-collar links.")
+        print(f"{animalNumber * iterations} data logs.")
         
     except Exception as e:
         session.rollback()
