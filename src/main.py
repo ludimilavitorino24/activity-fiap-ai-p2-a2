3e3f421f4bda34b03a6286fb7157a845216ee497
brunoconterato <brunoconterato@gmail.com>
2024-10-11 03:28:44 -0300

    refact: fix main

--- src/main.py
+++ src/main.py
@@ -4,11 +4,10 @@ dotenv.load_dotenv()
 
 from alert_processing.process_alerts import process_alerts
 
-# from report_generation.fetch_data import fetch_data
+from report_generation.fetch_data import fetch_data
 
 if __name__ == "__main__":
     process_alerts("11-10-2024")
 
-    # print("Data for report:", result)
-    # result = fetch_data("01-10-2024")
-    # print(result)
+    result = fetch_data("11-10-2024")
+    print(result)
