3441a00f8d1df4a82891b7fbaca3bf6ed08411b6
Luis Emidio <luisfuturist@gmail.com>
2024-10-13 23:41:33 -0300

    wip: add database cleaning script
    Co-authored-by: Bruno Conterato <brunoconterato@users.noreply.github.com>

---
+++ scripts/clear-database.py
@@ -0,0 +1,21 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+
+from db import engine, DataLog, Species, Breed, Animal, Collar, AnimalCollar, Alert
+from sqlalchemy.orm import sessionmaker
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+session.query(Alert).delete()
+session.query(DataLog).delete()
+session.query(AnimalCollar).delete()
+session.query(Collar).delete()
+session.query(Animal).delete()
+session.query(Breed).delete()
+session.query(Species).delete()
+
+session.commit()
+session.close()
+
